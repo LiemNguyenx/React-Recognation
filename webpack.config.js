0e2b6585e908eca9d0ad08aa3880b4e07f7c6684
liemnguyen <liemnguyenx@gmail.com>
2018-06-03 07:53:22 +0700

    mmmm

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,31 @@
+var webpack = require('webpack');
+var path = require('path');
+
+//var BUILD_DIR = path.resolve(__dirname, 'C:\\Users\\liemn\\Desktop\\React-Recognation\\build');
+
 var config = {
     entry: './index.js',
     output: {
-       path:'/',
-       filename: 'index.js',
+        path: '/',
+        filename: 'index.js',
+        // path: BUILD_DIR,
+        // filename: 'bundle1.js'
     },
     devServer: {
-       inline: true,
-       port: 6969
+        inline: true,
+        port: 3001
     },
     module: {
-       loaders: [
-          {
-             test: /\.jsx?$/,
-             exclude: /node_modules/,
-             loader: 'babel-loader',
-             query: {
-                presets: ['es2015', 'react','stage-0']
-             }
-          }
-       ]
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react', 'stage-0']
+                }
+            }
+        ]
     }
- }
- module.exports = config;
+}
+module.exports = config;
